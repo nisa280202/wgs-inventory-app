0e1b183b043d0456b3c04c9204788ba91b50865c
Nisa Alivia <nisa.alivia28@gmail.com>
2023-12-13 10:40:14 +0700

    chore: Add auth route in server.js

--- inventory-app-api/server.js
+++ inventory-app-api/server.js
@@ -1,11 +1,12 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 require('dotenv').config()
-const goodsRoutes = require('./route/goodsRoutes')
-const detailGoodsRoutes = require('./route/detailGoodsRoutes')
-const userRoutes = require('./route/userRoutes')
-const transactionRoutes = require('./route/transactionRoutes')
-const detailTransactionRoutes = require('./route/detailTransactionRoutes')
+const goodsRoutes = require('./route/goods')
+const detailGoodsRoutes = require('./route/detailGoods')
+const userRoutes = require('./route/user')
+const transactionRoutes = require('./route/transaction')
+const detailTransactionRoutes = require('./route/detailTransaction')
+const authRoutes = require('./route/auth')
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
@@ -18,6 +19,7 @@ app.use('/detail_goods', detailGoodsRoutes)
 app.use('/user', userRoutes)
 app.use('/transaction', transactionRoutes)
 app.use('/detail_transaction', detailTransactionRoutes)
+app.use('/auth', authRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on Port ${process.env.PORT}`);
